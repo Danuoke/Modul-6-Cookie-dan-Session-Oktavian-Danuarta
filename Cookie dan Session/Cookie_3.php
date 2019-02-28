fef49dc77be6c0c42cc087ef4dd6e9d3dce13847
Danuoke <43231270+Danuoke@users.noreply.github.com>
2019-02-28 19:37:46 +0700

    Add files via upload

---
+++ Cookie dan Session/Cookie_3.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<title>Hapus cookie</title>
+	</head>
+	<body>
+		<?php
+			setcookie('username','Oktavian1234');
+			if(isset($_COOKIE['username'])){
+				echo 'cookie di set<br/>';
+				//menghapus cookie dengan masa berlaku 1 jam yang lalu
+				setcookie('username','', time() - 1 * 3600);
+				echo 'cookie dihapus';
+			}else{
+				echo 'unset';
+			}
+		?>
+		<p>Tekan Refresh(f5);
+	</body>
+</html>
